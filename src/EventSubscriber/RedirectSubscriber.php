fecc47329d32da619a2a220b723e34fa7720c822
Stéphane Menut <smenut@dawan.fr>
2022-03-29 14:18:07 +0200

    fix return type

--- src/EventSubscriber/RedirectSubscriber.php
+++ src/EventSubscriber/RedirectSubscriber.php
@@ -34,7 +34,7 @@ class RedirectSubscriber implements EventSubscriberInterface
         }
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             'kernel.request' => [
